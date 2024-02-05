d5cbe9f71e08751a2ef641bb5fdef474681d6b96
Valinohtar <pmusial13@gmail.com>
2024-02-05 02:55:54 +0100

    Something with showing slots doesn't work properly

--- slot_machine.py
+++ slot_machine.py
@@ -1,6 +1,59 @@
+import random
+
+
 MAX_LINES = 3
 MIN_BET = 1
 MAX_BET = 100
+
+ROWS = 3
+COLS = 3
+
+symbol_count ={
+    "A" : 2,
+    "B" : 4,
+    "C" : 6,
+    "D" : 8
+}
+
+symbol_value ={
+    "A" : 5,
+    "B" : 4,
+    "C" : 3,
+    "D" : 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    for line in range(lines):
+        symbol = columns[0][line]
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = [[], [], []]
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+    
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns)-1:
+                print(column[row], "|", end="\n")
+            else:
+                print(column[row], end="")
+        print()
+
 
 def deposit():
     while True:
@@ -44,8 +97,17 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough balance.")
+        else:
+            break
     print(f"You are betting ${bet} on {lines} lines. Total bet: {lines*bet}")
 
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
